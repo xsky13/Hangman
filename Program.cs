51bbd6fef903d4c39718d412dcfe43e25fff560c
Lenny Maritnez <lennymartinez782@gmail.com>
2025-04-14 18:28:47 -0300

    no agrege nada

--- Program.cs
+++ Program.cs
@@ -4,9 +4,47 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Lenny");
-            Console.WriteLine("Segundo intento");
+
+        }
+        static void PlayGame()
+        {
+            string[] words = { "apple", "banana", "cherry" };
+            Random random = new Random();
+            string wordToGuess = words[random.Next(words.Length)];
+            string guessedWord = new string('_', wordToGuess.Length);
+            int attemptsLeft = 6;
+            while (attemptsLeft > 0 && guessedWord != wordToGuess)
+            {
+                Console.WriteLine($"Word to guess: {guessedWord}");
+                Console.WriteLine($"Attempts left: {attemptsLeft}");
+                Console.Write("Enter a letter: ");
+                char guessedLetter = Console.ReadKey().KeyChar;
+                Console.WriteLine();
+                if (wordToGuess.Contains(guessedLetter))
+                {
+                    for (int i = 0; i < wordToGuess.Length; i++)
+                    {
+                        if (wordToGuess[i] == guessedLetter)
+                        {
+                            guessedWord = guessedWord.Remove(i, 1).Insert(i, guessedLetter.ToString());
+                        }
+                    }
+                }
+                else
+                {
+                    attemptsLeft--;
+                    Console.WriteLine($"Wrong guess! '{guessedLetter}' is not in the word.");
+                }
+            }
+            if (guessedWord == wordToGuess)
+            {
+                Console.WriteLine($"Congratulations! You've guessed the word: {wordToGuess}");
+            }
+            else
+            {
+                Console.WriteLine($"Game over! The word was: {wordToGuess}");
+            }
         }
     }
+
 }
