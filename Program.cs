f45322038efe25208fd8d60de8f6d25a52a3c47f
Lenny Maritnez <lennymartinez782@gmail.com>
2025-04-14 17:54:47 -0300

    ahi va de nuevo

--- Program.cs
+++ Program.cs
@@ -5,6 +5,8 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Console.WriteLine("Lenny");
+            Console.WriteLine("Segundo intento");
         }
     }
 }
