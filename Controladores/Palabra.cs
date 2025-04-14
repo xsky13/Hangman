a0965f047fd53445e4fa3e428fbf9af56c31b28b
Jared Peter <aljapeter05@gmail.com>
2025-04-14 18:27:08 -0300

    agregar controlador de palabras

---
+++ Controladores/Palabra.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hangman.Controladores
+{
+    internal class Palabra
+    {
+
+
+        public static void Agregar()
+        {
+
+        }
+
+        public static void Seleccionar()
+        {
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
