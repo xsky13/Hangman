76b2d7311b5f180d4c174c4e915d340977f760db
Abiel2310 <abielm2310@gmail.com>
2025-04-14 22:58:09 -0300

    fuap

--- Controladores/AhorcadoController.cs
+++ Controladores/AhorcadoController.cs
@@ -8,10 +8,21 @@ namespace Hangman.Controladores
 {
     internal class AhorcadoController
     {
-        
+
+        public static string[] Espacios = Enumerable.Repeat("_", PalabraController.palabra.Length).ToArray();
+
         public static void Dibujar()
         {
-            // bruno
+            
+        }
+
+        public static void ImprimirEpacios(int indice, string letra)
+        {
+            if (indice != -1) Espacios[indice] = letra;
+            foreach (string e in Espacios)
+            {
+                Console.Write(e);
+            }
         }
 
         public static void Ingresar()
@@ -19,7 +30,8 @@ namespace Hangman.Controladores
             // abiel
             Console.Write("Ingresa letra: ");
             string nuevaLetra = Console.ReadLine();
-            PalabraController.Verificar(nuevaLetra);
+            int indicePalabra = PalabraController.Verificar(nuevaLetra);
+            ImprimirEpacios(indicePalabra, nuevaLetra);
         }
     }
 }
--- obj/Debug/net9.0/Hangman.AssemblyInfo.cs
+++ obj/Debug/net9.0/Hangman.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hangman")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+908c45d9fad8aa9cfd0d3625485591395af6d999")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1948686b4978e3fcb3691337e66d2d86081b34f4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hangman")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hangman")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
